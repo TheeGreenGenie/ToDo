ebdb5dcdd92be3885ea669593604790b866901c6
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-18 12:42:49 -0700

    function names, init variables, & main function

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import json
 import os
 from datetime import datetime
+
+class TodoList:
+    def __init__(self, file_path="todo_data.json"):
+        self.file_path = file_path
+        self.tasks = self._load_tasks()
+
+    def _load_tasks(self):
+        pass
+
+    def _save_tasks(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def list_tasks(self, show_completed=False):
+        pass
+
+    def complete_task(self, task_id):
+        pass
+
+    def delete_task(self, task_id):
+        pass
+
+    def print_help():
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
